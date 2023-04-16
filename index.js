1db17f3b8d0a4dee38875ecd54b1490f3986b4cc
Jhoana <jhoanazamora2@gmail.com>
2023-04-16 15:48:20 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,24 @@
+import cipher from "./cipher.js";
+const botonCifrar = document.querySelector("#cifrar");
+const botonDescifrar = document.querySelector("#descifrar");
+const botonReiniciar = document.querySelector("#botonReiniciar");
+
+botonCifrar.addEventListener("click", function () {
+  const offset = document.querySelector("#offset").value;
+  const mensajeI = document.querySelector("#mensajeI").value;
+  const mensajeEncode = cipher.encode(parseInt(offset), mensajeI);
+  document.querySelector("#mensajeF").value = mensajeEncode;
+});
+
+botonDescifrar.addEventListener("click", function () {
+  const offset = document.querySelector("#offset").value;
+  const mensajeI = document.querySelector("#mensajeI").value;
+  const mensajeDecode = cipher.decode(parseInt(offset), mensajeI); //OBTENER EL MENSAJE DESCIFRADO
+  document.querySelector("#mensajeF").value = mensajeDecode;
+});
+
+botonReiniciar.addEventListener("click", function () {
+  document.querySelector("#offset").value = "";
+  document.querySelector("#mensajeI").value = "";
+  document.querySelector("#mensajeF").value = "";
+});
